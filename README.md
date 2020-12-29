415180d2b316ba59c1e0530e7b3d03e014353209
Duyb <1255248666@qq.com>
2020-12-29 18:28:15 +0800

    koa study

---
+++ README.md
@@ -0,0 +1,58 @@
+# [koa](https://www.koajs.com.cn/)
+
+> [koa](https://github.com/koajs/koa) 应用是一个包含一系列中间件 generator 函数的对象。 这些中间件函数基于 request 请求以一个类似于栈的结构组成并依次执行。
+
+> [middleware](https://github.com/koajs/koa/wiki#middleware)
+
+> [koa-docs-Zh-CN](https://github.com/demopark/koa-docs-Zh-CN)
+
+> [Egg.js](https://github.com/eggjs/egg)
+
+## Installation
+
+```sh
+# Koa requires node v7.6.0 or higher for ES2015 and async function support.
+npm install koa
+```
+
+## Server
+
+```js
+const Koa = require('koa');
+const app = new Koa();
+
+// response
+app.use(ctx => {
+  ctx.body = 'Hello Koa';
+});
+
+app.listen(3000);
+```
+
+## [Middleware](https://github.com/demopark/koa-docs-Zh-CN/blob/master/guide.md)
+
+### `async` functions (node v7.6+)
+
+```js
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+```
+
+### Common function
+
+```js
+// Middleware normally takes two parameters (ctx, next), ctx is the context for one request,
+// next is a function that is invoked to execute the downstream middleware. It returns a Promise with a then function for running code after completion.
+
+app.use((ctx, next) => {
+  const start = Date.now();
+  return next().then(() => {
+    const ms = Date.now() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+  });
+});
+```
---
+++ app.js
@@ -0,0 +1,130 @@
+/*
+ * [koa](https://www.koajs.com.cn/)
+ * @Author: Duyb
+ * @Date: 2020-12-29 14:18:09
+ * @Last Modified by: Duyb
+ * @Last Modified time: 2020-12-29 18:11:00
+ */
+
+const fs = require('fs');
+const path = require('path');
+const Koa = require('koa');
+const compose = require('koa-compose');
+const serve = require('koa-static');
+
+const app = new Koa();
+
+// ! static
+const publicFiles = serve(path.join(__dirname, 'public'));
+publicFiles._name = 'static /public';
+
+app.use(publicFiles);
+
+// ! [Middleware](https://github.com/koajs/koa#middleware)
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`> : ${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// [中间件参数](https://github.com/demopark/koa-docs-Zh-CN/blob/master/guide.md#%E4%B8%AD%E9%97%B4%E4%BB%B6%E5%8F%82%E6%95%B0)
+function logger(format) {
+  format = format || ':method ":url"';
+
+  return async function (ctx, next) {
+    const str = format.replace(':method', ctx.method).replace(':url', ctx.url);
+
+    console.log(str);
+
+    await next();
+  };
+}
+
+app.use(logger());
+app.use(logger(':method :url'));
+
+async function random(ctx, next) {
+  if (ctx.path === '/random') {
+    ctx.body = Math.floor(Math.random() * 10);
+  } else {
+    await next();
+  }
+}
+
+async function backwards(ctx, next) {
+  if (ctx.path === '/backwards') {
+    ctx.body = 'sdrawkcab';
+  } else {
+    await next();
+  }
+}
+
+async function pi(ctx, next) {
+  if (ctx.path === '/pi') {
+    ctx.body = String(Math.PI);
+  } else {
+    await next();
+  }
+}
+
+const all = compose([random, backwards, pi]);
+
+app.use(all);
+
+// ! read dir
+app.use(async function (ctx, next) {
+  const paths = await fs.readdir('docs');
+  const files = await Promise.all(
+    paths.map(path => fs.readFile(`docs/${path}`, 'utf8'))
+  );
+
+  ctx.type = 'markdown';
+  ctx.body = files.join('');
+});
+
+app.use(async ctx => {
+  // console.log('>>> ctx :', ctx);
+  // console.log('\n>>> Object.entries(ctx) : ', Object.entries(ctx), '\n');
+  // console.log(
+  //   '\n>>> 与 Request 等价： ctx.path : ',
+  //   ctx.path,
+  //   ', ctx.request.path :',
+  //   ctx.request.path,
+  //   '\n'
+  // );
+  // console.log('>>> 应用实例引用 ctx.app :', ctx.app);
+
+  // console.log('>>> Koa 的 Request 对象 ctx.request :', ctx.request);
+  // console.log('>>> Koa 的 Response 对象 ctx.response :', ctx.response);
+
+  // console.log('>>> ctx.path :', ctx.path);
+  // console.log('>>> ctx.method  :', ctx.method);
+  // console.log('>>> node IncomingMessage ctx.req  :', ctx.req);
+  // console.log('>>> node ServerResponse ctx.res  :', ctx.res);
+
+  // 推荐的命名空间，用于通过中间件传递信息到前端视图
+  // ctx.state.user = await User.find(id);
+  // console.log('>>> ctx.state  :', ctx.state);
+
+  // ctx.cookies.set(name, value, [options])
+  ctx.cookies.set('name', 'du');
+  const cookieOfName = ctx.cookies.get('name');
+  console.log("cookieOfName ctx.cookies.get('name') :", cookieOfName);
+
+  // request.is(types...)
+  // console.log("ctx.is('html') :", ctx.is('html'));
+
+  // ! 中间件决定响应请求，并希望绕过下游中间件可以简单地省略 next()
+  ctx.body = 'hello world !';
+});
+
+console.log('app.env :', app.env);
+console.log('app.proxy :', app.proxy);
+console.log('app.subdomainOffset  :', app.subdomainOffset);
+
+app.listen(3000);
+// 等价于
+// http.createServer(app.callback()).listen(3000);
+// 这意味着您可以同时支持 HTTPS 和 HTTPS，或者在多个端口监听同一个应用。
+// https.createServer(app.callback()).listen(3001);
